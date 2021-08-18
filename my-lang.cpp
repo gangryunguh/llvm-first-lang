f00deb07ccab6f097d48c480b49e3852e6aa0a72
Gang-Ryung <gangryunguh@gmail.com>
2021-08-18 16:13:46 -0500

    AST

--- my-lang.cpp
+++ my-lang.cpp
@@ -3,6 +3,7 @@
 //
 #include <string>
 #include <iostream>
+#include <vector>
 
 // The lexer returns tokens [0-255] if it is an unknown character, otherwise one
 // of these for known things.
@@ -66,9 +67,69 @@ static int gettok() {
     return ThisChar;
 }
 
+class ExprAST {
+public:
+    virtual ~ExprAST() {}
+};
+
+class NumberExprAST: public ExprAST {
+    double Val;
+public:
+    NumberExprAST(double Val) : Val(Val) {}
+};
+
+class VariableExprAST: public ExprAST {
+    std::string Name;
+public:
+    VariableExprAST(const std::string &Name) : Name(Name) {}
+};
+
+class BinaryExprAST: public ExprAST {
+    char Op;
+    std::unique_ptr<ExprAST> LHS, RHS;
+public:
+    BinaryExprAST(char Op,
+                  std::unique_ptr<ExprAST> LHS,
+                  std::unique_ptr<ExprAST> RHS):
+                  Op(Op), LHS(std::move(LHS)), RHS(std::move(RHS)) {}
+};
+
+class CallExprAST: public ExprAST {
+    std::string Callee;
+    std::vector<std::unique_ptr<ExprAST>> Args;
+public:
+    CallExprAST(const std::string &Callee,
+                std::vector<std::unique_ptr<ExprAST>> Args)
+                : Callee(Callee), Args(std::move(Args)) {}
+};
+
+class PrototypeAST {
+    std::string Name;
+    std::vector<std::string> Args;
+
+public:
+    PrototypeAST(const std::string &name, std::vector<std::string> Args)
+    : Name(name), Args(std::move(Args)) {}
+
+    const std::string &getName() const { return Name; }
+};
+
+/// FunctionAST - This class represents a function definition itself.
+class FunctionAST {
+    std::unique_ptr<PrototypeAST> Proto;
+    std::unique_ptr<ExprAST> Body;
+
+public:
+    FunctionAST(std::unique_ptr<PrototypeAST> Proto,
+                std::unique_ptr<ExprAST> Body)
+                : Proto(std::move(Proto)), Body(std::move(Body)) {}
+};
+
+#if 0
 int main() {
     while (true) {
         int tok = gettok();
         std::cout << "got token: " << tok << std::endl;
     }
 }
+#endif // 0
